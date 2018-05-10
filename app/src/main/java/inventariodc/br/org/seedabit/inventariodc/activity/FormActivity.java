ca4c8c7ffd4e3f317ac00685c9514619d5b6ab73
wallsmanBR <alves_thiagobastos@hotmail.com>
2018-05-10 13:31:58 -0300

    finalizacao do layout do formulario de tombamento

--- app/src/main/java/inventariodc/br/org/seedabit/inventariodc/activity/FormActivity.java
+++ app/src/main/java/inventariodc/br/org/seedabit/inventariodc/activity/FormActivity.java
@@ -6,7 +6,9 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,19 +21,46 @@ import inventariodc.br.org.seedabit.inventariodc.interfaces.OnProductListener;
 public class FormActivity extends AppCompatActivity implements OnProductListener  {
 
     private TextView txtBarCode;
+    private EditText edtDescricao;
+    private EditText edtLocalizacao;
+    private EditText edtResponsavel;
+    private EditText edtObservacao;
     private Button btnScanner;
+    private Button btnLimpar;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
         this.txtBarCode = (TextView) findViewById(R.id.tVCodigoBarra);
+        this.edtDescricao = (EditText) findViewById(R.id.edtDescricao);
+        this.edtLocalizacao = (EditText) findViewById(R.id.edtLocalizacao);
+        this.edtResponsavel = (EditText) findViewById(R.id.edtResponsavel);
+        this.edtObservacao = (EditText) findViewById(R.id.edtObs);
         this.btnScanner = (Button) findViewById(R.id.btnScanner);
+        this.btnLimpar = (Button) findViewById(R.id.btnLimpar);
+        this.btnCadastrar = (Button) findViewById(R.id.btnSubmeter);
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //click do botao para scanear o codigo de barras
         this.btnScanner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 barcodeCapture();
+            }
+        });
+
+        //click do botao para limpar os campos do formulario
+        this.btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtBarCode.setText("");
+                edtDescricao.setText("");
+                edtLocalizacao.setText("");
+                edtResponsavel.setText("");
+                edtObservacao.setText("");
             }
         });
 
@@ -40,13 +69,10 @@ public class FormActivity extends AppCompatActivity implements OnProductListener
     @Override
     public void barcodeCapture() {
         IntentIntegrator integrator = new IntentIntegrator(FormActivity.this);
-//        intent.setBeepEnabled(true);
-//        intent.setCameraId(0);
-//        intent.initiateScan();
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-        integrator.setPrompt("Scan");
+        integrator.setPrompt("Escanear Número de Tombamento do Item");
         integrator.setCameraId(0);
-        integrator.setBeepEnabled(false);
+        integrator.setBeepEnabled(true);
         integrator.setBarcodeImageEnabled(false);
         integrator.initiateScan();
     }
